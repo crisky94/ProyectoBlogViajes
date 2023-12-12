c39e930082b49a208f0293759df390d79d5fe4e6
German Robles <germanroobles@gmail.com>
2023-12-12 21:23:25 +0100

    register user

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-import express from "express";
+import express from 'express';
 const app = express();
-import morgan from "morgan";
-import dotenv from "dotenv";
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 dotenv.config();
-import getPool from "./src/db/getPool.js";
+import getPool from './src/db/getPool.js';
+import routes from './src/routes/index.js';
 
 app.use(express.json());
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`El servidor esta escuchando en el puerto ${process.env.PORT}...`)
-})
+// Middleware que indica a express dónde están las rutas.
+app.use(routes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `El servidor esta escuchando en el puerto ${process.env.PORT}...`
+  );
+});
