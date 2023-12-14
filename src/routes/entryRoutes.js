9d67eef1af1953b3044870ed60cc225662151eca
German Robles <germanroobles@gmail.com>
2023-12-14 20:53:53 +0100

    votar entradas y arreglos

--- src/routes/entryRoutes.js
+++ src/routes/entryRoutes.js
@@ -1,8 +1,11 @@
-
 import newEntryController from '../controllers/entries/newEntryController.js';
 import deleteEntryController from '../controllers/entries/deleteEntryController.js';
-import entryExistsController from '../middleware/entryExistsController.js';
+// import entryExistsController from '../middleware/entryExistsController.js';
 
+import voteEntryController from '../controllers/entries/voteEntryController.js';
+import authUserController from '../middleware/authUserController.js';
+import userExistsController from '../middleware/userExistsController.js';
+import entryExistsController from '../middleware/entryExistsController.js';
 
 import express from 'express';
 const router = express.Router();
@@ -11,11 +14,28 @@ import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 
 //ruta para crear una recomendacion con foto
-router.post('/entries', upload.single('image'), newEntryController,
-    entryExistsController
+router.post(
+  '/entries',
+  upload.single('image'),
+  newEntryController,
+  entryExistsController
 );
 
 //ruta para borrar la foto y la recomendacion
-router.delete('/entries', upload.single(''), deleteEntryController, entryExistsController);
+router.delete(
+  '/entries',
+  upload.single(''),
+  deleteEntryController,
+  entryExistsController
+);
+
+// Votar una entrada.
+router.post(
+  '/entries/:entryId/votes',
+  authUserController,
+  userExistsController,
+  entryExistsController,
+  voteEntryController
+);
 
 export default router;
