986cff3b93e86acb40e76c2698aebf8c95b1e4ce
Cris Labrador <criistiiniilla.1994@gmail.com>
2023-12-14 19:37:40 +0100

    newEntrySchema

--- src/routes/entryRoutes.js
+++ src/routes/entryRoutes.js
@@ -1,26 +1,21 @@
 
 import newEntryController from '../controllers/entries/newEntryController.js';
- import deleteEntryController from '../controllers/entries/deleteEntryController.js';
- import selectEntryById from '../../selectEntryById.js'
+import deleteEntryController from '../controllers/entries/deleteEntryController.js';
+import entryExistsController from '../middleware/entryExistsController.js';
 
-import express from 'express'
+
+import express from 'express';
 const router = express.Router();
+
 import multer from 'multer';
-const upload = multer({ dest: 'uploads/'});
+const upload = multer({ dest: 'uploads/' });
 
-
+//ruta para crear una recomendacion con foto
 router.post('/entries', upload.single('image'), newEntryController,
-
+    entryExistsController
 );
 
-router.get("/recomendaciones/place/:entriesPlace", selectEntryById)
-
-router.delete('/entries', upload.single(''), deleteEntryController)
-
-  
-
-
-
-
+//ruta para borrar la foto y la recomendacion
+router.delete('/entries', upload.single(''), deleteEntryController, entryExistsController);
 
 export default router;
