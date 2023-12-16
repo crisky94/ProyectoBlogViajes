8ed55461234b6be87f2bc2e675e0387fa8a454a3
Cris Labrador <criistiiniilla.1994@gmail.com>
2023-12-16 12:21:43 +0100

    agregado authUserController en delete y post entry

--- src/routes/entryRoutes.js
+++ src/routes/entryRoutes.js
@@ -17,6 +17,7 @@ const upload = multer({ dest: 'uploads/' });
 router.post(
   '/entries',
   upload.single('image'),
+  authUserController,
   newEntryController,
   entryExistsController
 );
@@ -25,6 +26,7 @@ router.post(
 router.delete(
   '/entries',
   upload.single(''),
+  authUserController,
   deleteEntryController,
   entryExistsController
 );
