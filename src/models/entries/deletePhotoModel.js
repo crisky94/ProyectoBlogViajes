b391dfeadcdc6841d43b25803b1feba483223855
Cris Labrador <criistiiniilla.1994@gmail.com>
2023-12-16 11:54:30 +0100

    newEntryController req.user.id

--- src/models/entries/deletePhotoModel.js
+++ src/models/entries/deletePhotoModel.js
@@ -2,13 +2,9 @@ import getPool from '../../db/getPool.js';
 const deletePhotoModel = async (entryId) => {
     const pool = await getPool();
 
- 
-
     await pool.query(`DELETE FROM entryPhotos WHERE entryId = ?`,
         [entryId]);
-
-    
-
+        
 };
 
 export default deletePhotoModel;
