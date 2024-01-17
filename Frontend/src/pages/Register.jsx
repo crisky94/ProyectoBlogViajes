c52a86b6d52fce2a8341ec2f4bdba4129149f04a
German Robles <germanroobles@gmail.com>
2024-01-17 20:20:46 +0100

    Registro usuario

---
+++ Frontend/src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import FormRegister from "../components/FormRegister";
+
+const Register = () => {
+    return (
+        <>
+            <h2>Registrarse</h2>
+            <FormRegister />
+        </>
+    );
+};
+
+export default Register;
---
+++ Frontend/src/services/userRegisterSevice.js
@@ -0,0 +1,24 @@
+const registerUserService = async ({ username, email, password }) => {
+    const url = `${import.meta.env.VITE_API_URL}users/register`;
+
+    try {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, email, password }),
+        });
+
+        if (!res.ok) {
+            const json = await res.json();
+            throw new Error(json.message);
+        }
+
+        return await res.json();
+    } catch (error) {
+        throw new Error(`Error registering user: ${error.message}`);
+    }
+};
+
+export default registerUserService;
