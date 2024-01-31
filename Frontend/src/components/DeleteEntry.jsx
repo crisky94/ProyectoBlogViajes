9e7c2027251ec75615012b9f28e983e14f7a29fb
German Robles <germanroobles@gmail.com>
2024-01-31 19:03:59 +0100

    Token

--- Frontend/src/components/DeleteEntry.jsx
+++ Frontend/src/components/DeleteEntry.jsx
@@ -1,50 +1,51 @@
-import  { useState } from 'react';
+import { useState } from "react";
 import deleteEntryService from "../services/deleteEntryService";
 
+const DeleteEntry = ({ id }) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const [error, setError] = useState("");
 
+    const DeletePopup = ({ show, onDelete, onCancel }) => {
+        if (!show) return null;
 
+        return (
+            <div className="popup">
+                <div className="popup-inner">
+                    <h3>
+                        ¿Estás segurx de que quieres borrar esta publicación?
+                    </h3>
+                    <button onClick={onDelete}>Sí, borrar</button>
+                    <button onClick={onCancel}>Cancelar</button>
+                </div>
+            </div>
+        );
+    };
+    const handleDelete = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            await deleteEntryService({ id, token });
+            console.log("Publicacion borrada con exito!");
+        } catch (error) {
+            setError(error.message);
+        }
+    };
 
+    const handleCancel = () => {
+        setShowPopup(false);
+    };
 
-const DeleteEntry = ({id}) => {
- 
-  const DeletePopup = ({ show, onDelete, onCancel }) => {
-    if (!show) return null;
-  
     return (
-      <div className="popup">
-        <div className="popup-inner">
-          <h3>¿Estás segurx de que quieres borrar esta publicación?</h3>
-          <button onClick={onDelete}>Sí, borrar</button>
-          <button onClick={onCancel}>Cancelar</button>
+        <div>
+            <button onClick={() => setShowPopup(true)}>
+                Borrar Publicación
+            </button>
+            <DeletePopup
+                show={showPopup}
+                onDelete={handleDelete}
+                onCancel={handleCancel}
+            />
         </div>
-      </div>
     );
-  };
-  const [showPopup, setShowPopup] = useState(false);
-  const [error, setError] = useState("");
-
-  const handleDelete = async () => {
-   
-    try{
-       await deleteEntryService({id});
-       console.log("Publicacion borrada con exito!");
-    }catch(error){
-      setError(error.message)
-    }
-  };
-  
-  const handleCancel = () => {
-    setShowPopup(false); 
-  };
-
-  return (
-    <div>
-    
-      <button onClick={() => setShowPopup(true)}>Borrar Publicación</button>
-      <DeletePopup show={showPopup} onDelete={handleDelete} onCancel={handleCancel} />
-    </div>
-  );
 };
 
 export default DeleteEntry;
-
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,23 +1,23 @@
-import FormLogin from "../components/FormLogin";
-import { useState } from "react";
+// import FormLogin from "../components/FormLogin";
+// import { useState } from "react";
 
-const App = () => {
-    const [loggedInUser, setLoggedInUser] = useState(null);
+// const Login = () => {
+//     const [loggedInUser, setLoggedInUser] = useState(null);
 
-    const handleLogin = ({ usuario, token }) => {
-        setLoggedInUser({ usuario, token });
-    };
+//     const handleLogin = ({ usuario, token }) => {
+//         setLoggedInUser({ usuario, token });
+//     };
 
-    return (
-        <div>
-            <h1>Logeado!</h1>
-            {loggedInUser ? (
-                <p>Bienvenido</p>
-            ) : (
-                <FormLogin onLogin={handleLogin} />
-            )}
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <h1>Logeado!</h1>
+//             {loggedInUser ? (
+//                 <p>Bienvenido</p>
+//             ) : (
+//                 <FormLogin onLogin={handleLogin} />
+//             )}
+//         </div>
+//     );
+// };
 
-export default App;
+// export default Login;
