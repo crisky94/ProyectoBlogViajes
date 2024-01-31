13e581f025ef593a65ba0311895a8cf045085876
German Robles <germanroobles@gmail.com>
2024-01-31 19:54:05 +0100

    Guardar Token en la sesion

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -24,6 +24,15 @@ const App = () => {
         localStorage.removeItem("token");
         setLoggedInUser(null);
     };
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            setLoggedInUser({
+                token: token,
+            });
+        }
+    }, []);
 
     return (
         <>
